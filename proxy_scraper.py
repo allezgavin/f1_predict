12dd14a0d4d61017150b396e946e6dbd1a577d08
Gavin Xia <gx1@williams.edu>
2024-04-01 05:57:22 -0400

    all content scraper implemented

--- proxy_scraper.py
+++ proxy_scraper.py
@@ -4,20 +4,20 @@
 # In[1]:
 
 
+import csv
+import numpy as np
+import pandas as pd
 import datetime
-MINIMUM_UPDATE_INTERVAL = datetime.timedelta(days=1)
-src = 'sportsbook_data/fanduel.csv'
 
 
 # In[2]:
 
 
-import csv
-import numpy as np
-import pandas as pd
+MINIMUM_UPDATE_INTERVAL = datetime.timedelta(days=1)
+src = 'sportsbook_data/fanduel.csv'
 
 
-# In[6]:
+# In[3]:
 
 
 from seleniumwire import webdriver
@@ -34,39 +34,8 @@ options = {
     }
 }
 
-# We use sel_driver instead of the usual nomenclature driver to avoid conflict with racing driver
-sel_driver = webdriver.Chrome(seleniumwire_options=options)
 
-
-# In[7]:
-
-
-url = 'https://sportsbook.fanduel.com/motorsport?tab=f1-race-props'
-
-try:
-    sel_driver.get(url)
-    sel_driver.implicitly_wait(100) # Wait for dynamically loaded content
-    sel_driver.find_element('xpath', '//div[@aria-label="Show more"]').click()
-    tab = sel_driver.find_element('xpath',
-                              '//ul[contains(., " Grand Prix") and contains(., "Podium Finish")][not(.//ul)]')
-    podium_texts = tab.text.split('\n')
-    
-except NoSuchElementException as n:
-    print('Cannot find the element. Error message as below:\n', n)
-
-except Exception as e:
-    print("An error occurred:\n", e)
-
-
-# In[5]:
-
-
-time = pd.Timestamp.now()
-race = podium_texts[0]
-podium_texts = podium_texts[2:-2]
-
-
-# In[ ]:
+# In[4]:
 
 
 def read_last_line(file_path):
@@ -78,7 +47,7 @@ def read_last_line(file_path):
         return lines[-1]
 
 
-# In[ ]:
+# In[5]:
 
 
 def calculate_elapsed_time(last_update_time):
@@ -89,16 +58,142 @@ def calculate_elapsed_time(last_update_time):
     return elapsed_time
 
 
-# In[ ]:
+# In[6]:
 
 
-def update_src(file_path):
+def starts_with_any(string, substrings):
+    for substring in substrings:
+        if string.startswith(substring):
+            return True
+    return False
+
+
+# In[7]:
+
+
+def update1_src(sel_driver, file_path, url = 'https://sportsbook.fanduel.com/motorsport?tab=formula-1'):
+    sel_driver.get(url)
+    sel_driver.implicitly_wait(100)
+
+    show_more_buttons = sel_driver.find_elements('xpath', '//div[@aria-label="Show more"]')
+    for button in show_more_buttons:
+        button.click()
+    
+    tab = sel_driver.find_element('xpath',
+                              '//ul[contains(., " Grand Prix") and contains(., "F1 Outrights")][not(.//ul)]')
+    
+    rates_text = tab.text.split('\n')
+    
     with open(file_path, mode='a', newline='') as file:
         writer = csv.writer(file)
+        time = pd.Timestamp.now()
+        race = rates_text[0]
+        i = 0
+        
+        if rates_text[1] == 'Outright Betting':
+            i += 2
+            while (i < len(rates_text)) and not rates_text[i+1].startswith('Show'):
+                driver = rates_text[i]
+                value = rates_text[i+1]
+                try:
+                    value = float(value)
+                except Exception as e:
+                    print("Invalid value:\n", value)
+                    break
+                new_row = [race, 'win', time, driver, value]
+                writer.writerow(new_row)
+                i += 2
+                
+            print("Fanduel outright betting data updated successfully.")
+        
+        i += 2
+        
+        if rates_text[i] == 'F1 Outrights':
+            outright_items = ['F1 Drivers Championship','F1 Constructors Championship', 'Betting Without']
+            item = ''
+            i += 1
+            while (i < len(rates_text)) and not rates_text[i].startswith('Overall H2H'):
+                if rates_text[i+1].startswith('Show'):
+                    i += 2
+                    continue
+                if starts_with_any(rates_text[i], outright_items):
+                    item = rates_text[i]
+                    i += 1
+                    continue
+                driver = rates_text[i]
+                value = rates_text[i+1]
+                try:
+                    value = float(value)
+                except Exception as e:
+                    print("Invalid value:\n", value)
+                    break
+                new_row = [race, item, time, driver, value]
+                writer.writerow(new_row)
+                i += 2
+                
+            print("Fanduel season-long data updated successfully.")
+                
+        if rates_text[i].startswith('Overall H2H'):
+            while (i+5 < len(rates_text)):
+                item = rates_text[i]
+                
+                driver = rates_text[i+2]
+                value = rates_text[i+3]
+                try:
+                    value = float(value)
+                except Exception as e:
+                    print("Invalid value:\n", value)
+                    break
+                new_row = [race, item, time, driver, value]
+                writer.writerow(new_row)
+                
+                driver = rates_text[i+4]
+                value = rates_text[i+5]
+                try:
+                    value = float(value)
+                except Exception as e:
+                    print("Invalid value:\n", value)
+                    break
+                new_row = [race, item, time, driver, value]
+                writer.writerow(new_row)
+                
+                i += 7
+    
+            print('Fanduel H2H data updated successfully.')
 
+
+# In[8]:
+
+
+def update2_src(sel_driver, file_path, url = 'https://sportsbook.fanduel.com/motorsport?tab=f1-race-props'):
+    try:
+        sel_driver.get(url)
+        sel_driver.implicitly_wait(100) # Wait for dynamically loaded content
+        sel_driver.find_element('xpath', '//div[@aria-label="Show more"]').click()
+        tab = sel_driver.find_element('xpath',
+                                  '//ul[contains(., " Grand Prix") and contains(., "Podium Finish")][not(.//ul)]')
+        podium_texts = tab.text.split('\n')
+        race = podium_texts[0]
+        podium_texts = podium_texts[2:-2]
+
+    except NoSuchElementException as n:
+        print('Cannot find the element. Error message as below:\n', n)
+
+    except Exception as e:
+        print("An error occurred:\n", e)
+        
+    with open(file_path, mode='a', newline='') as file:
+        writer = csv.writer(file)
+        time = pd.Timestamp.now()
+        
         for i in range(len(podium_texts)//2):
             driver = podium_texts[2*i]
             value = podium_texts[2*i+1]
+            try:
+                value = float(value)
+            except Exception as e:
+                print("Invalid value:\n", value)
+                break
             new_row = [race, 'podium finish', time, driver, value]
             writer.writerow(new_row)
 
@@ -106,17 +201,38 @@ def update_src(file_path):
     # pd.read_csv(src).tail()
 
 
-# In[ ]:
+# In[9]:
 
 
-last_line = read_last_line(src)
-last_update_time = last_line[2]
-elapsed_time = calculate_elapsed_time(last_update_time)
+def update_src_all(file_path):
+    # We use sel_driver instead of the usual nomenclature driver to avoid conflict with racing driver
+    sel_driver = webdriver.Chrome(seleniumwire_options=options)
+    
+    update1_src(sel_driver, file_path)
+    update2_src(sel_driver, file_path)
+    
+    sel_driver.quit()
 
-if elapsed_time >= MINIMUM_UPDATE_INTERVAL:
-    update_src(src)
+
+# In[10]:
+
+
+test_mode = False
+
+if test_mode:
+    src = 'sportsbook_data/fanduel_test.csv'
+    update_src_all(src) # forced update
+
 else:
-    print("No need to update the CSV file.")
+    last_line = read_last_line(src)
+    last_update_time = last_line[2]
+    elapsed_time = calculate_elapsed_time(last_update_time)
+
+    if elapsed_time >= MINIMUM_UPDATE_INTERVAL:
+        update_src_all(src)
+
+    else:
+        print("No need to update the CSV files.")
 
 
 # Run this in the command line to compile the .py file for task scheduling
